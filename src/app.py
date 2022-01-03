4f344923356e216aeb745946784bf38b8c6c9af7
unknown <aidani8youssef@gmail.com>
2022-01-03 18:02:31 +0300

    Index page updated

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return '<h1>Hello world!</h1><h3>It is nice to meet you man</h3>'
 
 if __name__ == '__main__':
     app.run()
